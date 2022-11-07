0be1187c97db0b90250aeed0494aba55d34bc66b
mateofelgueras <mateofelgueras@gmail.com>
2022-11-07 10:46:55 -0300

    Rutas

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Game from '../views/Game.vue'
+import Registro from "@/components/Registro";
+import Desierto from "@/components/Desierto";
+import InformacionCuenta from "@/components/InformacionCuenta";
+import tundra from "@/components/Tundra";
+import selva from "@/components/Selva";
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/game',
-    name: 'game',
-    component: Game
-  },
+  {path: '/', name: 'home', component: HomeView},
+  {path: '/registro', name: "registro", component: Registro},
+  {path: '/game', name: 'game', component: Game},
+  {path: '/desierto', name:'desierto', component: Desierto},
+  {path: '/informacioncuenta', name:'informacioncuenta', component: InformacionCuenta},
+  {path: '/tundra', name:'tundra', component: tundra},
+  {path: '/selva', name:'selva', component: selva}
 ]
 
 const router = createRouter({
