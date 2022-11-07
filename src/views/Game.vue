14619959f70014b4d3048f575a8434e0f9870cc6
mateofelgueras <mateofelgueras@gmail.com>
2022-11-07 20:37:21 -0300

    Update Game.vue

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,11 +1,21 @@
 <template>
+  <Header v-bind:show-button="false"/>
   <p>Proximamente el juego....</p>
+  <Footer/>
 </template>
 
 <script>
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
 export default {
-  name: "Game"
+  name: "Game",
+  components:{
+    Header,
+    Footer
+  }
 }
+
 </script>
 
 <style scoped>
