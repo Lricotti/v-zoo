0d7a0f46d5eafa12df9baec02e122bb18748412f
mateofelgueras <mateofelgueras@gmail.com>
2022-11-08 22:23:45 -0300

    poco de todo

---
+++ src/components/RegistoExitoso.vue
@@ -0,0 +1,47 @@
+<template>
+
+  <div id="todo">
+    <Header v-bind:show-button="false"/>
+    <span class="titulo">¡{{ this.$route.query["clientName"] }}!</span>
+    <span class="titulo"> Gracias por el  registro, ya pertences a la comunidad de V-Zoo</span>
+
+    <div id="imagen-registro">
+      <img src="../assets/img/zooo.webp" alt="">
+    </div>
+
+    <router-link :to="{ name: 'home'}">Volver</router-link>
+    <Footer></Footer>
+  </div>
+
+</template>
+
+<script>
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+export default {
+  name: "Registroexito",
+  components: {Footer, Header}
+}
+</script>
+
+<style scoped>
+#todo {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  height: 98vh;
+}
+#imagen-registro {
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+}
+#sub-success-img img {
+  border-radius: 5px;
+}
+.titulo {
+  margin: 10px;
+  font-size: 200%;
+  font-weight: bold;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -7,17 +7,22 @@ import InformacionCuenta from "@/components/InformacionCuenta";
 import tundra from "@/components/Tundra";
 import selva from "@/components/Selva";
 import Oceano from "@/components/Oceano";
+import RegistoExitoso from "@/components/RegistoExitoso";
+import paginaNoEncontrada from "@/components/PaginaNoEncontrada";
 
 
 const routes = [
   {path: '/', name: 'home', component: HomeView},
   {path: '/registro', name: "registro", component: Registro},
+  {path: '/registo/exitoso', name: "registroexito", component: RegistoExitoso},
   {path: '/game', name: 'game', component: Game},
   {path: '/desierto', name:'desierto', component: Desierto},
   {path: '/informacioncuenta', name:'informacioncuenta', component: InformacionCuenta},
   {path: '/tundra', name:'tundra', component: tundra},
   {path: '/selva', name:'selva', component: selva},
-  {path: '/oceano', name:'oceano', component: Oceano}
+  {path: '/oceano', name:'oceano', component: Oceano},
+
+  {path: '/:pathMatch(.*)*', component: paginaNoEncontrada}
 
 ]
 
